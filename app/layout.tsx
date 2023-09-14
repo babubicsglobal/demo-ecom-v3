581c062567dfd5a3c97c63f8c6a543b6830bd114
Vijayakanth M <vijayakanth@bicsglobal.com>
2023-09-14 16:36:29 +0530

    meta data issues fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,23 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "../components/header/index";
 import { SessionProvider } from "next-auth/react";
-const inter = Inter({ subsets: ["latin"] });
+
 import { ReactNode } from "react";
 import Providers from "./../Providers";
+
+const inter = Inter({ subsets: ["latin"] });
 interface IProps {
   children: ReactNode;
 }
+
+export const metadata = {
+  title: "Bics Store",
+  description: "Bics Store Ecommerce Application",
+};
 export default function RootLayout({ children }: IProps) {
   return (
     <html lang="en">
-      <body>
+      <body className="inter.className">
         <Providers>
           <Header />
           {children}
